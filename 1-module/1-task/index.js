27f66938b32d90c1c60ecc6c51492d287b057623
Michail Lemeshev <MilemX11@gmail.com>
2023-01-17 21:09:26 +0500

    Lesson 1. Factorial calculation

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,10 @@
 function factorial(n) {
-  // ваш код...
+  let factorialResult = n;
+
+  if(n === 0) return 1
+  for(let i = 1; i < n; i++){
+    factorialResult *= i
+  }
+
+  return factorialResult
 }
