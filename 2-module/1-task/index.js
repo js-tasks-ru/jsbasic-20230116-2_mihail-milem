7b571f06232adfef3fbaf3ec851f040fecfd2563
Michail Lemeshev <MilemX11@gmail.com>
2023-01-24 21:56:20 +0500

    2module Amount Salaries

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let amountSalaries = 0;
+
+  for(let key in salaries){
+    if(Number.isInteger(salaries[key])) amountSalaries += salaries[key]
+  }
+
+  return amountSalaries
 }
