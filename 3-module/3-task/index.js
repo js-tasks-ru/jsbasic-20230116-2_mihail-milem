4a5483e5de0a635ee6f3f7e10bc57e5996efd80b
Michail Lemeshev <MilemX11@gmail.com>
2023-01-28 23:17:57 +0500

    3module.3task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  let strSplit = str.split('-')
+  let newStr = [strSplit[0]]
+    
+  for(let i = 1; i < strSplit.length; i++){
+    newStr.push(strSplit[i][0].toUpperCase() + strSplit[i].slice(1))
+  }
+
+    return newStr.join('')
 }
