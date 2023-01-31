3ba099c7b1529c8f28f5eae4566f2c1b7fa37c4c
Michail Lemeshev <MilemX11@gmail.com>
2023-01-31 22:19:41 +0500

    4module.1task FriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,15 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const wrapperListFriends = document.createElement('ul');
+
+
+  for(let i = 0; i < friends.length; i++){
+    const friendsList = document.createElement('li');
+
+    wrapperListFriends.append(friendsList);
+
+    friendsList.textContent = `${friends[i].firstName} ${friends[i].lastName}`
+  }
+  
+  
+  return wrapperListFriends
 }
