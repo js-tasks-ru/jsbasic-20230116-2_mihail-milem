7013dd9b71e9045570bdbc5f8837758448bfff6b
Michail Lemeshev <MilemX11@gmail.com>
2023-02-03 19:33:17 +0500

    5module.2task - toggle text

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
-  // ваш код...
+  const toggleBtn = document.querySelector('.toggle-text-button');
+  const toggleTextBtn = document.querySelector('#text')
+
+  toggleBtn.addEventListener('click', () => { 
+    (toggleTextBtn.hasAttribute('hidden')) ? toggleTextBtn.hidden = '' : toggleTextBtn.hidden = 'true'
+    
+  })
 }
