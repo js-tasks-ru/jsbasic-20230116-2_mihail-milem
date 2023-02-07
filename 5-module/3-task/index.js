0b3442ac667ddb25a0b90a4f8abd278d24b20d8a
Michail Lemeshev <MilemX11@gmail.com>
2023-02-07 21:50:09 +0500

    Refactoring slider

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,65 +1,36 @@
 function initCarousel() {
-  let activeSlide = 1
-  let nextWithSlide = 0
-  const carouselInner = document.querySelector('.carousel__inner');
-  const carouselArrowRight = document.querySelector('.carousel__arrow_right');
-  const carouselArrowLeft = document.querySelector('.carousel__arrow_left');
-  const slideWidth = carouselInner.offsetWidth;
-  const slideMaximumAmount = carouselInner.children.length;
+    const carouselInner = document.querySelector('.carousel__inner');
+    const carouselArrowRight = document.querySelector('.carousel__arrow_right');
+    const carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+    const slideWidth = carouselInner.offsetWidth;
+    let activeSlide = 1;
+    let nextWithSlide = 0;
 
-  if(activeSlide) carouselArrowLeft.style.display = 'none'
+    function handleClick(direction) {
+      if(direction === 'right'){
+        nextWithSlide -= slideWidth
+        activeSlide++
+      }
 
-  carouselArrowRight.addEventListener('click', () => {
-    nextWithSlide -= slideWidth
-    activeSlide++
-    (activeSlide === 1) ? carouselArrowLeft.style.display = 'none' : carouselArrowLeft.style.display = '';
-    (activeSlide === slideMaximumAmount) ? carouselArrowRight.style.display = 'none' : carouselArrowRight.style.display = '';
-    carouselInner.style.transform = `translateX(${nextWithSlide}px)`
-  });
+      if(direction === 'left'){
+        nextWithSlide += slideWidth
+        activeSlide--
+      }
+  
+      (activeSlide === carouselInner.children.length) ? carouselArrowRight.style.display = 'none' : carouselArrowRight.style.display = '';
+      (activeSlide === 1) ? carouselArrowLeft.style.display = 'none' : carouselArrowLeft.style.display = '';
 
-  carouselArrowLeft.addEventListener('click', () => {
-    nextWithSlide += slideWidth
-    activeSlide--
-    (activeSlide === 1) ? carouselArrowLeft.style.display = 'none' : carouselArrowLeft.style.display = '';
-    (activeSlide === slideMaximumAmount) ? carouselArrowRight.style.display = 'none' : carouselArrowRight.style.display = '';
-    carouselInner.style.transform = `translateX(${nextWithSlide}px)`
-    
-  });
+      carouselInner.style.transform = `translateX(${nextWithSlide}px)`;
+    }
+
+    if(activeSlide) carouselArrowLeft.style.display = 'none';
+
+    carouselArrowRight.addEventListener('click', () => {
+      handleClick('right')
+    });
+  
+    carouselArrowLeft.addEventListener('click', () => {
+      handleClick('left')
+    });
+
 }
-
-// Мое решение(не подошло для тестов)
-
-// let activeSlide = 1
-// let nextSlide = 0
-// const carouselInner = document.querySelector('.carousel__inner');
-// const carouselArrowRight = document.querySelector('.carousel__arrow_right');
-// const carouselArrowLeft = document.querySelector('.carousel__arrow_left');
-// const carouselWidth = carouselInner.offsetWidth;
-
-// carouselArrowRight.addEventListener('click', () => {
-//   initCarousel('right')
-// });
-
-// carouselArrowLeft.addEventListener('click', () => {
-//   initCarousel('left')
-// });
-
-// function initCarousel(direction) {
-  
-//   if(direction === 'right'){
-//     nextSlide -= carouselInner.offsetWidth
-//     activeSlide++
-//   }
-
-//   if(direction === 'left'){
-//     nextSlide += carouselInner.offsetWidth
-//     activeSlide--
-//   }
-  
-//   (activeSlide === 4) ? carouselArrowRight.style.display = 'none' : carouselArrowRight.style.display = '';
-//   (activeSlide === 1) ? carouselArrowLeft.style.display = 'none' : carouselArrowLeft.style.display = '';
-
-//   carouselInner.style.transform = `translateX(${nextSlide}px)`
-  
-// }
-
